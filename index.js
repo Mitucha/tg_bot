949ea99a3014f8b6fe0d9a95fd3d7902fe43e553
Michael <majkl.orexov@bk.ru>
2024-07-11 23:22:34 +0300

    исправление ошибок

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 require('dotenv').config()
 const sequelize = require('./db')
 const { UserModel, AttestationModel, ResultModel } = require('./models')
-
 const {Bot, InlineKeyboard} = require('grammy')
-// Импровизированная база данных
+
+// Импровизированная база данных и переменные для счетчиков
+
+const attestationItems = [
+    {title: 'Аттестация Июня', data: 'qwerty', command: 'att1'},
+    {title: 'Аттестация Августа', data: 'qwerty', command: 'att2'}
+]
+
+function commandAtt(arr){
+    let result = []
+    for (let i = 0; i < arr.length; i++){
+        result.push(
+            {command: arr[i].command, description: arr[i].title}
+        )
+    }
+    return result
+}
+
 const db = [
     {
       label: 'В какие блюда входит болгарский перец?',
@@ -31,14 +47,21 @@ const db = [
       sucs: 4
     }
   ]
-const bot = new Bot(process.env.BOT_API_KEY)
+let leng = 0
+let cookies = 0 // Хардкодим Печеньки тут, чтобы, по окончанию теста, отправить их на сервер
+//==============================
+const bot = new Bot(process.env.BOT_API_KEY) //Подключение к боту
 
-bot.api.setMyCommands([
+//Необходимые пользовательские команды
+
+bot.api.setMyCommands(commandAtt(attestationItems))
+
+/*bot.api.setMyCommands([
     {
         command: 'attestation',
         description: 'Пройти аттестацию'
     }
-])
+])*/
 
 // Подключение  к базе данных
 async function conectDB() {
@@ -54,7 +77,7 @@ conectDB()
 // Старт бота
 bot.command('start', async stx => {
 
-    await stx.reply('Привет, давай пройдем небольшую аттестацию, проверим твои знания. За каждый правильный ответ ты получишь «печеньку»')
+    await stx.reply('Привет, давай пройдем небольшую аттестацию, проверим твои знания. За каждый правильный ответ ты получишь «печеньку»🍪')
     await stx.reply("Напиши свое Имя и Фамилию")
 })
 // Определяем по выбору пользователя, кем он является в команде
@@ -69,46 +92,61 @@ async function sequenceOfQuestions(ctx, num){
     await ctx.reply(context.label, {reply_markup: inlineKeyboard})
 }
 
-let cookies = 0
+// Функция для определения правильного варианта ответа на вопрос теста
 function isDone(number, doneNum){
-    if(number == doneNum){
+    if(number === doneNum){
         return 'done'
     }
     return 'notDone'
 }
 
-
-let leng = 0
+// Вызов теста
 bot.callbackQuery('button-1', async ctx => {
     sequenceOfQuestions(ctx, leng++)
     //const inlineKeyboard = new InlineKeyboard().text('14см', 'button-3').row().text('15см', 'button-4')
     //await ctx.reply('Какой длины у вас член? ', {reply_markup: inlineKeyboard})
 })
 
+// Обработчик не верного ответа пользователя
 bot.callbackQuery(['notDone'], async ctx => {
-    if(leng + 1 > db.length){
-        await ctx.reply(`Вы заработали ${cookies}🍪`)
-        return leng = 0
-    } 
-    sequenceOfQuestions(ctx, leng++)
+    if(leng == db.length){
+        try{
+            //const User = await UserModel.findOne({where: {telegramm_id: ctx.from.id}})
+            //User.cookies = cookies
+            //await User.save()
+            //await ctx.reply(`Ты заработал ${User.cookies}🍪`)
+            leng = 0
+            await ctx.reply(`Ты заработал ${cookies}🍪`)
+            cookies = 0
+        } catch(e){console.error(e)}
+        
+    }else{sequenceOfQuestions(ctx, leng++)}
+    
 })
 
-bot.callbackQuery(['done',], async ctx => {
+// Обработчик верного ответа пользователя
+bot.callbackQuery('done', async ctx => {
     cookies++
-    if(leng + 1 > db.length){
-        const User = await UserModel.findOne({where: {telegramm_id: ctx.message.from.id}})
-        User.cookies = cookies
-        await User.save()
-        await ctx.reply(`Ты заработал ${User.cookies}🍪`)
-        return leng = 0
-    } 
-    sequenceOfQuestions(ctx, leng++)
+    if(leng == db.length){
+        //const User = await UserModel.findOne({where: {telegramm_id: ctx.from.id}})
+        //User.cookies = cookies
+        //await User.save()
+        //await ctx.reply(`Ты заработал ${User.cookies}🍪`)
+        leng = 0
+        await ctx.reply(`Ты заработал ${cookies}🍪`)
+        cookies = 0
+    } else{sequenceOfQuestions(ctx, leng++)}
 })
 
 bot.callbackQuery('button-2', async ctx => {
     sequenceOfQuestions(ctx, leng++)
 })
+
+bot.callbackQuery('add_attestation', async ctx => {
+    ctx.reply('Я жду список')
+})
 //==============================================================
+
 bot.command('attestation', async (ctx) => {
     const inlineKeyboard = new InlineKeyboard().text('Кассир', 'button-1').text('Повар', 'button-2')
     await ctx.reply('Выберите свою позицию ', {reply_markup: inlineKeyboard})
@@ -116,20 +154,22 @@ bot.command('attestation', async (ctx) => {
 
 bot.on("message", async (ctx) => {
     // Есть ли пользователь в базе данных?
-    const isRegistredUser = await UserModel.findOne({
+    const registredUser = await UserModel.findOne({
         where: {
             telegramm_id: JSON.stringify(ctx.message.from.id)
         }
     })
 
     // Проверка на права администратора
-    if(ctx.from.id === 556854763 || ctx.from.id === 366486108) {
-        await ctx.reply("Здравствуй, хозяин")
+    if(ctx.from.id === 556854769 || ctx.from.id === 366486108) {
+        await ctx.reply("Здравствуй, администратор")
+        const inlineKeyboard = new InlineKeyboard().text('Добавить аттестацию', 'add_attestation').row().text('Результаты пользователей')
+        await ctx.reply('Какие будут действия?', {reply_markup: inlineKeyboard})
         return
     }
 
     // Пользователь новый или старый. Алгоритм действий
-    if(isRegistredUser === null) {
+    if(registredUser === null) {
         await UserModel.create({
             chatId: JSON.stringify(ctx.message.chat.id),
             name: ctx.message.text,
@@ -137,16 +177,13 @@ bot.on("message", async (ctx) => {
             compliteAtt: 0,
             cookies: 0
         })
+
         await ctx.reply(`Вот мы и познакомились, ${ctx.message.text}`)
 
-        // Выбор направления курса
-
     } else{
-        await ctx.reply(`Мы уже знакомы, ${isRegistredUser.name}. У тебя ${cookies} 🍪`)
+        //await ctx.reply(`Мы уже знакомы, ${registredUser.name}. У тебя ${cookies} 🍪.`)
         
-        // Выбор направления курса
-        
-        
+        await ctx.reply(registredUser)
     }
     
     
